81ad65d91907489521b9e33043f60529a4e16cbf
IMMontoya <Immontoya93@gmail.com>
2025-03-10 12:41:27 -0700

    Load system message

--- app.py
+++ app.py
@@ -19,6 +19,41 @@ login(token=huggingface_login, add_to_git_credential=True)
 checkpoint = "meta-llama/Llama-3.2-1B-Instruct"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
+# Read the few-shot interview prompt
+with open("few_shot_interview_prompt.txt") as f:
+    system_message = f.read()
+    
+# Select the number of examples for the few-shot learning
+def select_n_few_shot_examples(n, system_message):
+    
+    # Split the system message by lines
+    system_message_lines = system_message.split("\n")
+    new_system_message_lines = []
+    # Find the line that starts with "Example [n+1]:"
+    for i, line in enumerate(system_message_lines):
+        if line.startswith(f"Example {n+1}:"):
+            break
+        new_system_message_lines.append(line)
+        
+    # The last line is closes the system message
+    new_system_message_lines.append(system_message_lines[-1])
+    system_message = "\n".join(new_system_message_lines)
+    
+    return system_message
+
+system_message = select_n_few_shot_examples(3, system_message)
+    
+# Replace "//RESUME//" with the resume text
+with open("resume.txt") as f:
+    system_message = system_message.replace("//RESUME//", f.read())
+    
+# Print the number of tokes in the system message
+print(f"Number of tokens in the system message: {len(tokenizer(system_message)['input_ids'])}")
+
+# Print the max token length of the model
+print(f"Max token length of the model: {tokenizer.model_max_length}")
+
+
 # Define the pipeline
 text_generator = pipeline("text-generation", model=checkpoint, tokenizer=tokenizer)
 
