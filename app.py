d188a0ec79c7e0ad3a3d3108552fe8036b1eccb8
IMMontoya <Immontoya93@gmail.com>
2025-04-25 16:03:06 -0700

    Comment out donation request

--- app.py
+++ app.py
@@ -383,16 +383,18 @@ Try copy/pasting a job description to talk about a specific role.
 
 Connect with me on [Linkedin](https://www.linkedin.com/in/isaiah-montoya/). <br>
 Checkout the [Github Repo](https://github.com/IMMontoya/instant_interview_bot).
-
-Consider [donating](https://www.paypal.com/ncp/payment/NF8NSLUCBLVUS) to support this project:
-
-<style>.pp-NF8NSLUCBLVUS{text-align:center;border:none;border-radius:0.25rem;min-width:11.625rem;padding:0 2rem;height:2.625rem;font-weight:bold;background-color:#FFD140;color:#000000;font-family:"Helvetica Neue",Arial,sans-serif;font-size:1rem;line-height:1.25rem;cursor:pointer;}</style>
-<form action="https://www.paypal.com/ncp/payment/NF8NSLUCBLVUS" method="post" target="_blank" style="display:inline-grid;justify-items:center;align-content:start;gap:0.5rem;">
-  <input class="pp-NF8NSLUCBLVUS" type="submit" value="Donate via PayPal" style="font-size: 1.5rem; color: black; background-color: yellow; border-radius: 0.25rem;" />
-  <img src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" />
 </form>
 </div>
 """,
+
+############### DONATION BUTTON #################### 
+# Consider [donating](https://www.paypal.com/ncp/payment/NF8NSLUCBLVUS) to support this project:
+
+# <style>.pp-NF8NSLUCBLVUS{text-align:center;border:none;border-radius:0.25rem;min-width:11.625rem;padding:0 2rem;height:2.625rem;font-weight:bold;background-color:#FFD140;color:#000000;font-family:"Helvetica Neue",Arial,sans-serif;font-size:1rem;line-height:1.25rem;cursor:pointer;}</style>
+# <form action="https://www.paypal.com/ncp/payment/NF8NSLUCBLVUS" method="post" target="_blank" style="display:inline-grid;justify-items:center;align-content:start;gap:0.5rem;">
+#   <input class="pp-NF8NSLUCBLVUS" type="submit" value="Donate via PayPal" style="font-size: 1.5rem; color: black; background-color: yellow; border-radius: 0.25rem;" />
+#   <img src="https://www.paypalobjects.com/images/Debit_Credit_APM.svg" alt="cards" />
+  
     chatbot=gr.Chatbot(placeholder="<div style='text-align: center;'>This is a chatbot designed to provide instant interview responses as if it were me (Isaiah Montoya).</div>",
     type="messages"),
     flagging_mode="manual",
